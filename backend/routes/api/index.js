8cefb047c8a0d9cc5f4d808cbf96f74d5d301d02
Will Torres <w.torresjr@gmail.com>
2023-08-02 18:51:58 -0400

    Added user login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
-const {
-  setTokenCookie,
-  restoreUser,
-  requireAuth,
-} = require("../../utils/auth.js");
-
-const { User } = require("../../db/models");
+const sessionRouter = require("./session");
+const usersRouter = require("./users");
+const { restoreUser } = require("../../utils/auth");
 
 router.use(restoreUser);
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+router.get("/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
 
 module.exports = router;
