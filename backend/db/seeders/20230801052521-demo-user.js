1dcadb568c4541de20740b2a39827631eff43e53
Will Torres <w.torresjr@gmail.com>
2023-08-03 17:40:55 -0400

    created seedUsers array and used bulkCreate instead of insert

--- backend/db/seeders/20230801052521-demo-user.js
+++ backend/db/seeders/20230801052521-demo-user.js
@@ -6,38 +6,40 @@ if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
+const seedUsers = [
+  {
+    email: "demo@user.io",
+    username: "Demo-lition",
+    hashedPassword: bcrypt.hashSync("password"),
+    firstName: "FakeFirst1",
+    lastName: "FakeLast1",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    email: "user1@user.io",
+    username: "FakeUser1",
+    hashedPassword: bcrypt.hashSync("password2"),
+    firstName: "FakeFirst2",
+    lastName: "FakeLast2",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    email: "user2@user.io",
+    username: "FakeUser2",
+    hashedPassword: bcrypt.hashSync("password3"),
+    firstName: "FakeFirst3",
+    lastName: "FakeLast3",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     options.tableName = "Users";
-    return queryInterface.bulkCreate(options, [
-      {
-        email: "demo@user.io",
-        username: "Demo-lition",
-        hashedPassword: bcrypt.hashSync("password"),
-        firstName: "FakeFirst1",
-        lastName: "FakeLast1",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        email: "user1@user.io",
-        username: "FakeUser1",
-        hashedPassword: bcrypt.hashSync("password2"),
-        firstName: "FakeFirst2",
-        lastName: "FakeLast2",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        email: "user2@user.io",
-        username: "FakeUser2",
-        hashedPassword: bcrypt.hashSync("password3"),
-        firstName: "FakeFirst3",
-        lastName: "FakeLast3",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    return queryInterface.bulkCreate(options, seedUsers);
   },
 
   down: async (queryInterface, Sequelize) => {
